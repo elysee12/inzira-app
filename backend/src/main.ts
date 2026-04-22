4515ecb2221a08ca1c3ff43664b033e4ea527fe6
elysee12 <tuyisengeelysee1@gmail.com>
2026-04-22 23:35:09 +0200

    Fix CORS and detailed error messages

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,11 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
